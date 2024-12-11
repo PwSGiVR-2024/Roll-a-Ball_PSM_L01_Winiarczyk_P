182f0ed273c1f8ee12058d3029dcc4814623a1a3
Mikaki02 <patrykwiniary01@tlen.pl>
2024-12-11 21:30:59 +0100

    Kończenie level2, oraz rework level 3
    Dodano podstawy do platformówki w level 2, oraz naprawiono kamere w level 3 oraz zmieniony sposóbb działania poziomu (Inny kod na grawitację oraz poruszanie)

---
+++ Assets/Scripts/Level3/FauxGravityAtractor.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class FauxGravityAtractor : MonoBehaviour
+{
+    public float gravity = -15;
+
+    public void Attract(Transform body)
+    {
+        Vector3 gravityUp = (body.position - transform.position).normalized;
+        Vector3 bodyup = body.up;
+
+        body.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
+
+        
+        Quaternion targetRotation = Quaternion.FromToRotation(bodyup, gravityUp) * body.rotation;
+        body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 50 * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Level3/PlayerController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movespeed = 15;
+    private Vector3 moveDir;
+
+    void Update()
+    {
+        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDir) * movespeed * Time.deltaTime);
+    }
+}
